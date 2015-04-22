8f3ca520872f44a458f0faf739ee5c003b3715fc
rmshree <rmshree@users.noreply.github.com>
2015-04-22 12:15:08 -0700

    Update VirtualMachine.cpp

--- VirtualMachine.cpp
+++ VirtualMachine.cpp
@@ -4,17 +4,23 @@
 
 extern "C"{
 
+int volatile counter;
+
+void AlarmCB(void *param){
+        for (counter;counter > 0; counter--);   //need count down timers for each sleeping thread (?)      
+}//AlarmCB
 
 TVMStatus VMStart(int tickms, int machinetickms, int argc, char *argv[]){
 	const char *arg = argv[1];
 	TVMMainEntry ret = VMLoadModule(arg);		
+	counter = tickms;
 	
 	MachineInitialize(1000);	
-	MachineRequestAlarm(1000);	//need to write callback func
+	MachineRequestAlarm(1000, AlarmCB, NULL);       //FYI could pass in a DataStructure instead of NULL
 	MachineEnableSignals();
 	
 	TMachineFileCallback callback
 	//VMMain(); 
-}
+}//VMStart
 
 }//end of extern
