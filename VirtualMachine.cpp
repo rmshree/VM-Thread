532cfbfec50e894b2673bdd8455511d256c09f23
Surti Sundaresan <sundares@pc25.cs.ucdavis.edu>
2015-04-22 15:41:18 -0700

    works for hello.so

--- VirtualMachine.cpp
+++ VirtualMachine.cpp
@@ -1,26 +1,45 @@
 #include "VirtualMachine.h"
-#include "VirtualMachineUtils.c"
 #include "Machine.h"
 
 extern "C"{
-//:{D
 int volatile counter;
+
+TVMMainEntry VMLoadModule(const char *module);
 
 void AlarmCB(void *param){
         for (counter;counter > 0; counter--);   //need count down timers for each sleeping thread (?)      
 }//AlarmCB
 
 TVMStatus VMStart(int tickms, int machinetickms, int argc, char *argv[]){
-	const char *arg = argv[1];
+	const char *arg = argv[0];
+
 	TVMMainEntry ret = VMLoadModule(arg);		
 	counter = tickms;
 	
-	MachineInitialize(1000);	
-	MachineRequestAlarm(1000, AlarmCB, NULL);       //FYI could pass in a DataStructure instead of NULL
-	MachineEnableSignals();
+	//MachineInitialize(1000);	
+	//MachineRequestAlarm(1000, AlarmCB, NULL);       //FYI could pass in a DataStructure instead of NULL
+	//MachineEnableSignals();
 	
-	TMachineFileCallback callback
-	//VMMain(); 
+	//TMachineFileCallback callback
+	if(ret == NULL)
+		return(VM_STATUS_FAILURE);
+	else{
+		ret(argc, argv);
+		return(VM_STATUS_SUCCESS);
+	}
 }//VMStart
+
+
+TVMStatus VMFileWrite(int filedescriptor, void *data, int *length){	//needs to work with machine something?
+	if(data == NULL || length == NULL)
+		return(VM_STATUS_ERROR_INVALID_PARAMETER);
+	else{
+		write(filedescriptor, data, *length);
+		return(VM_STATUS_SUCCESS);
+	}
+
+}
+
+
 
 }//end of extern
